33dab51bf3705190af36fe86e7da0c82e7e693c4
Lauren <lauren.a.heavey@gmail.com>
2022-11-13 17:18:46 -0600

    Add last stretch response.

--- assignment/scripts/part-supply.js
+++ assignment/scripts/part-supply.js
@@ -80,3 +80,15 @@ console.log(sum);
 //    no more boxes can be filled.
 //    Then log how many boxes were filled, and how many parts are left over.
 console.log('9. Filling boxes with a "while" loop');
+ 
+let parts = 572;
+let filledBoxes = 0;
+const boxMax = 7;
+
+while (parts > boxMax) {
+    parts -= 7;
+    filledBoxes ++;
+}
+
+console.log('Total filled boxes: ' + filledBoxes);
+console.log('Parts remaining: ' + parts%boxMax);
